943d5980c4d4bc01439f972b99dacc81699b0538
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-13 15:45:24 +0100

    Add Author type

--- the-net-ninja/server/schema/schema.js
+++ the-net-ninja/server/schema/schema.js
@@ -3,21 +3,38 @@ const {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
+  GraphQLID,
+  GraphQLInt,
 } = require('graphql');
 
 // dummy data
-var books = [
+const books = [
   { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
   { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
 ];
 
+const authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' },
+];
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -26,12 +43,18 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } }, // argument pass to query, here we use id to find the book
+      args: { id: { type: GraphQLID } }, // argument pass to query, here we use id to find the book
       resolve(parent, args) {
         // code to get data from db / other source
         const book = books.filter((book) => book.id == args.id)[0];
-        console.log(book);
         return book;
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return authors.filter((author) => author.id == args.id)[0];
       },
     },
   },
