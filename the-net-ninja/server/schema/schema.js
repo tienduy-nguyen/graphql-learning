06596de27fb659b7c6ad66b19dfefcf6b4cdddbf
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-13 12:18:40 +0100

    Root query

--- the-net-ninja/server/schema/schema.js
+++ the-net-ninja/server/schema/schema.js
@@ -5,13 +5,28 @@ const {
   GraphQLString,
 } = require('graphql');
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Book',
-    fields: () => ({
-      id: { type: GraphQLString },
-      name: { type: GraphQLString },
-      genre: { type: GraphQLString },
-    }),
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } }, // argument pass to query, here we use id to find the book
+      resolve(parent, args) {
+        // code to get data from db / other source
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
