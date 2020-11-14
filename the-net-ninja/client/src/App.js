65db20fca2a6231b32d125f28b1e7cd0b71c6e09
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-14 10:28:29 +0100

    Setup Apollo graphql

--- the-net-ninja/client/src/App.js
+++ the-net-ninja/client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import BookList from './components/BookList';
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div id='main'>
+        <h1>Ninja's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
