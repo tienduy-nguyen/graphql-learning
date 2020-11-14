43a176cf54dc837ef8d147fdf7c065f7bc5d77b0
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-14 10:44:12 +0100

    Making request from React

--- the-net-ninja/server/index.js
+++ the-net-ninja/server/index.js
@@ -3,10 +3,13 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
+// Allow cross-origin requests
+app.use(cors());
 
 connectDB();
 
@@ -22,7 +25,7 @@ app.get('/', (req, res) => {
   res.send('Hi there!');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log('Server is running on port ', port);
+  console.log('Server is running on port', port);
 });
